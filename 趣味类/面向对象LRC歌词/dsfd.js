6f8da746d6b4374b69f0c026a0ce27089b648dda
AngelYu89 <627513802@qq.com>
2016-06-27 17:35:09 +0800

    eye.clock.music.@key

---
+++ 趣味类/面向对象LRC歌词/dsfd.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2016/6/20.
+ */
+// var haha=document.getElementById('audio');
+// console.log(haha.currentTime);
+// function aaa(){
+//     console.log(haha.currentTime);
+// }
+// setInterval(aaa,1000);
