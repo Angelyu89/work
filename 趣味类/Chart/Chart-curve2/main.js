bd449730de006eb1f3f722650746d4dc7b390b5e
AngelYu89 <627513802@qq.com>
2016-06-29 17:14:51 +0800

    2016.6.29

---
+++ 趣味类/Chart/Chart-curve2/main.js
@@ -0,0 +1,51 @@
+/**
+ * Created by sks on 2016/6/29.
+ */
+
+(function(){
+    function Main(){
+        this._MAX_VALUE=100;
+        //this._data=[10,11,13,60,30,18,23,30,56,43,20,36];
+
+        this._data=[{name:"01",value:"10",fillColor:"#ff00ff"},
+                    {name:"02",value:"10",fillColor:"#ff0000"},
+                    {name:"03",value:"10",fillColor:"#0000ff"},
+                    {name:"04",value:"10",fillColor:"#ff00ff"},
+                    {name:"05",value:"10",fillColor:"3ff00ff"},
+                    {name:"06",value:"10",fillColor:"#ff00ff"},
+                    {name:"07",value:"10",fillColor:"#ff00ff"},
+                    {name:"08",value:"10",fillColor:"#ff00ff"},
+                    {name:"09",value:"10",fillColor:"#ff00ff"},
+                    {name:"10",value:"10",fillColor:"#ff00ff"},
+                    {name:"11",value:"10",fillColor:"#ff00ff"},
+                    {name:"12",value:"10",fillColor:"#ff00ff"}
+
+        ];
+
+        this._context2d=document.getElementById("canvas").getContext("2d");
+
+        this.drawData();
+    }
+    
+    Main.prototype.drawData=function(){
+        //视图与人眼方向相反
+        //var y=800*(1-this._data[0]/this._MAX_VALUE);
+        var p=this.getPositionByIndex(0);
+        this._context2d.moveTo(p.x,p.y);
+        
+        for(var i=1;i<this._data.length;i++){
+            p=this.getPositionByIndex(i);
+            this._context2d.lineTo(p.x,p.y);
+        }
+        this._context2d.stroke();
+    };
+    
+    Main.prototype.getPositionByIndex=function(index){
+        var y=500*(1-this._data[index]/this._MAX_VALUE);
+        
+        var x=(800/12)*index+20;
+        return{x:x,y:y};
+    };
+
+    new Main();
+}());
